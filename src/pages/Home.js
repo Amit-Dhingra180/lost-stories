4b6fc12a02b34cdd89b1286e7652340011a5749f
Amit Dhingra <amitdhingra180@gmail.com>
2024-07-16 20:16:51 +0530

    v4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div>Home Page</div>
+      <Link to='/review'>Click here for Review Page</Link>
+    </div>
+    
   )
 }
 
